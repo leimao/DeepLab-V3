c6a7ac04797fbdea1ff3ee898c1bff70b8cfe772
Lei Mao <dukeleimao@gmail.com>
2018-08-19 18:03:38 -0700

    update

--- utils.py
+++ utils.py
@@ -134,12 +134,14 @@ class Iterator(object):
 
         # Multithread image processing
         # Reference: https://www.kaggle.com/inoryy/fast-image-pre-process-in-parallel
-        results = Parallel(n_jobs = self.num_jobs)(delayed(self.process_func)((image_filename, label_filename)) for image_filename, label_filename in zip(image_filenames_minibatch, label_filenames_minibatch))
+
+        results = Parallel(n_jobs = self.num_jobs)(delayed(self.process_func)(image_filename, label_filename) for image_filename, label_filename in zip(image_filenames_minibatch, label_filenames_minibatch))
         images, labels = zip(*results)
+
         images = np.asarray(images)
         labels = np.asarray(labels)
         
-        return results, images
+        return images, labels
 
 
 def read_image(image_filename):
@@ -152,34 +154,176 @@ def read_label(label_filename):
 
     # Magic function to read VOC2012 semantic labels
     # https://github.com/tensorflow/models/blob/master/research/deeplab/datasets/remove_gt_colormap.py#L42
-    label = np.asarray(Image.open(label_file))
+    label = np.asarray(Image.open(label_filename))
     label = np.expand_dims(label, axis = 2)
 
     return label
 
 def subtract_channel_means(image, channel_means):
 
-    image_normalized = image - np.reshape(channel_means, (1,1,3))
+    return image - np.reshape(channel_means, (1,1,3))
 
-    return image_normalized
+def add_channel_means(image, channel_means):
 
+    return image + np.reshape(channel_means, (1,1,3))
+
+
+def flip_image(image, label):
+
+    image_flipped = np.fliplr(image)
+    label_flipped = np.fliplr(label)
+
+    return image_flipped, label_flipped
+
+def resize_image(image, label, output_size):
+
+    '''
+    output_size: [height, width]
+    '''
+
+    image_resized = cv2.resize(image, (output_size[1], output_size[0]), interpolation = cv2.INTER_LINEAR)
+    label_resized = cv2.resize(label, (output_size[1], output_size[0]), interpolation = cv2.INTER_NEAREST)
+    label_resized = np.expand_dims(label_resized, axis = 2)
+
+    return image_resized, label_resized
+
+
+def random_crop(image, label, output_size):
+
+    assert image.shape[0] >= output_size[0] and image.shape[1] >= output_size[1], 'image size smaller than the desired output size.'
+
+    height_start = np.random.randint(image.shape[0] - output_size[0] + 1)
+    width_start = np.random.randint(image.shape[1] - output_size[1] + 1)
+    height_end = height_start + output_size[0]
+    width_end = width_start + output_size[1]
+
+    image_cropped = image[height_start:height_end, width_start:width_end]
+    label_cropped = label[height_start:height_end, width_start:width_end]
+
+    return image_cropped, label_cropped
+
+
+def image_augmentaion(image, label, output_size, scale_factor = 1.5):
+
+    original_height = image.shape[0]
+    original_width = image.shape[1]
+    target_height = output_size[0]
+    target_width = output_size[1]
+
+    image_augmented = image.copy()
+    label_augmented = label.copy()
+
+    if original_height >= int(scale_factor * target_height) and original_width >= int(scale_factor * target_width):
+        image_augmented, label_augmented = random_crop(image = image_augmented, label = label_augmented, output_size = output_size)
+    else:
+        rescaled_size = [np.random.randint(target_height, int(scale_factor * target_height)), np.random.randint(target_width, int(scale_factor * target_width))]
+        image_augmented, label_augmented = resize_image(image = image_augmented, label = label_augmented, output_size = rescaled_size)
+        image_augmented, label_augmented = random_crop(image = image_augmented, label = label_augmented, output_size = output_size)
+
+    # Flip image and label
+    if np.random.random() > 0.5:
+        image_augmented, label_augmented = flip_image(image = image_augmented, label = label_augmented)
+
+    return image_augmented, label_augmented
 
 
 class DataPrerocess(object):
 
-    def __init__(self, height, width, channel_means):
+    def __init__(self, channel_means, output_size = [513, 513], scale_factor = 1.5):
 
-        self.height = height
-        self.width = width
         self.channel_means = channel_means
+        self.output_size = output_size
+        self.scale_factor = scale_factor
 
-    def preprocess_data(image_filename, label_filename):
+    def preprocess(self, image_filename, label_filename):
         # Read data from file
         image = read_image(image_filename = image_filename)
         label = read_label(label_filename = label_filename)
+        image, label = image_augmentaion(image = image, label = label, output_size = self.output_size, scale_factor = self.scale_factor)
+
         # Image normalization
         image = subtract_channel_means(image = image, channel_means = self.channel_means)
 
+        return image, label
+
+
+
+
+##################################################################################################################################
+'''
+The following image annotition saving codes are slightly modified from Google's official DeepLab repository.
+https://github.com/tensorflow/models/blob/master/research/deeplab/utils/get_dataset_colormap.py
+'''
+
+def bit_get(val, idx):
+    '''
+    Gets the bit value.
+    Args:
+    val: Input value, int or numpy int array.
+    idx: Which bit of the input val.
+    Returns:
+    The "idx"-th bit of input val.
+    '''
+    return (val >> idx) & 1
+
+
+
+def create_pascal_label_colormap():
+    '''
+    Creates a label colormap used in PASCAL VOC segmentation benchmark.
+    Returns:
+    A colormap for visualizing segmentation results.
+    Reference:
+    '''
+    colormap = np.zeros((256, 3), dtype = int)
+    ind = np.arange(256, dtype = int)
+
+    for shift in reversed(range(8)):
+        for channel in range(3):
+            colormap[:, channel] |= bit_get(ind, channel) << shift
+        ind >>= 3
+
+    return colormap
+
+def label_to_color_image(label):
+    '''
+    Adds color defined by the dataset colormap to the label.
+    Args:
+    label: A 2D array with integer type, storing the segmentation label.
+    dataset: The colormap used in the dataset.
+    Returns:
+    result: A 2D array with floating type. The element of the array is the color indexed by the corresponding element in the input label to the dataset color map.
+    Raises:
+    ValueError: If label is not of rank 2 or its value is larger than color map maximum entry.
+    '''
+    if label.ndim != 2:
+        raise ValueError('Expect 2-D input label')
+
+    if np.max(label) >= 256:
+        raise ValueError('label value too large.')
+
+    colormap = create_pascal_label_colormap()
+
+    return colormap[label]
+
+def save_annotation(label, filename, add_colormap = True):
+    '''
+    Saves the given label to image on disk.
+    Args:
+    label: The numpy array to be saved. The data will be converted to uint8 and saved as png image.
+    save_dir: The directory to which the results will be saved.
+    filename: The image filename.
+    add_colormap: Add color map to the label or not.
+    colormap_type: Colormap type for visualization.
+    '''
+    # Add colormap for visualizing the prediction.
+    if add_colormap:
+        colored_label = label_to_color_image(label)
+    else:
+        colored_label = label
+
+    pil_image = Image.fromarray(colored_label.astype(dtype = np.uint8))
+    pil_image.save(filename)
 
 
 
@@ -188,12 +332,45 @@ class DataPrerocess(object):
 
 
 
-def read_data_voc2012(image_filename, label_filename):
 
-    image = read_image(image_filename = image_filename)
-    label = read_label(label_filename = label_filename)
+if __name__ == '__main__':
+    
+    train_dataset = Dataset(dataset_filename = './data/VOCdevkit/VOC2012/train_dataset.txt', images_dir = './data/VOCdevkit/VOC2012/JPEGImages', labels_dir = './data/VOCdevkit/VOC2012/SegmentationClass', image_extension = '.jpg', label_extension = '.png')
+    print(train_dataset.image_filenames)
+    print(train_dataset.size)
+
+    channel_means = save_load_means(means_filename = './models/channel_means.npz', image_filenames = train_dataset.image_filenames, recalculate = False)
+    print(channel_means)
+
+    voc2012_preprocessor = DataPrerocess(channel_means = channel_means, output_size = [513, 513], scale_factor = 1.5)
 
 
-    return image, label
+    train_iterator = Iterator(dataset = train_dataset, minibatch_size = 16, process_func = voc2012_preprocessor.preprocess, random_seed = None, scramble = True, num_jobs = 8)
+
+    # Test iterator
+    '''
+    while True:
+        images, labels = train_iterator.next_minibatch()
+        print(images.shape, labels.shape)
+    '''
+
+    images, labels = train_iterator.next_minibatch()
+
+    print(images.dtype, labels.dtype)
+
+    for i, (image, label) in enumerate(zip(images, labels)):
+        image = add_channel_means(image = image, channel_means = channel_means)
+        print(type(image), image.shape)
+        print(type(label), label.shape)
+        label = np.squeeze(label, axis = -1)
+        print(type(label), label.shape)
+        save_annotation(label = label, filename = str(i) + '.png', add_colormap = True)
+        #label = Image.fromarray(image.astype(np.uint8))
+        #label.save(str(i) + '.png')
+
+
+        cv2.imwrite(str(i) + '.jpg', image)
+
+
 
 
